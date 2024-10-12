60d0f1d135ddb87cec7831e1d5bab32e865f9712
hsan-isetsf <hsan.isetsf@gmail.com>
2024-10-12 15:23:32 +0200

    build-commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Mon application Laravel</title>
+    <link href="{{ asset('frontend/assets/index-CZpCW9iA.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="root"></div>
