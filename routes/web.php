a0b9f35427f70fffdc13a06cbfa47d10640701b8
hsan-isetsf <hsan.isetsf@gmail.com>
2024-10-13 14:22:46 +0200

    commit-vercel

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
+/*
 Route::get('/{any}', function () {
     return view('welcome');
+})->where('any', '.*');
+*/
+Route::get('/{any}', function () {
+    if (File::exists(public_path($_SERVER["REQUEST_URI"]))) {
+        return File::get(public_path($_SERVER["REQUEST_URI"]));
+    }
+    return view('welcome'); 
 })->where('any', '.*');
