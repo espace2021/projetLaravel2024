0acdf0e6d7b36e360b51712fd310087542f87760
hsan-isetsf <hsan.isetsf@gmail.com>
2024-10-14 15:34:02 +0200

    vercel-commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Mon application Laravel</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="{{ secure_asset('frontend/assets/index-CZpCW9iA.css') }}" rel="stylesheet">
 </head>
 <body>
